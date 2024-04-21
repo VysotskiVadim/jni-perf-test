9fd5241625b67b3a89ffec3d73548cb1c1963983
vadzim.v <vysotski.vadim@gmail.com>
2024-04-21 16:08:00 +0200

    cleanup

--- benchmark/src/androidTest/java/dev/vadzimv/jniperftest/benchmark/JniBenchmark.kt
+++ benchmark/src/androidTest/java/dev/vadzimv/jniperftest/benchmark/JniBenchmark.kt
@@ -1,6 +1,5 @@
 package dev.vadzimv.jniperftest.benchmark
 
-import android.util.Log
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -10,12 +9,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Benchmark, which will execute on an Android device.
- *
- * The body of [BenchmarkRule.measureRepeated] is measured in a loop, and Studio will
- * output the result. Modify your code to see how it affects performance.
- */
 @RunWith(AndroidJUnit4::class)
 class JniBenchmark {
 
